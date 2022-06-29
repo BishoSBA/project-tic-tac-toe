fcce0bd314c902716acf44904c82574e4e2f0379
Beshir Saeed <beshir-saeed@live.com>
2022-06-29 12:10:50 +0300

    gameboard module initialized

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameBoard = (() => {
+	const board = new Array(9);
+})();
+
+const displayController = () => {
+	const update = () => {
+		const htmlBoard = document.querySelector("main");
+		const cells = [];
+	};
+};
